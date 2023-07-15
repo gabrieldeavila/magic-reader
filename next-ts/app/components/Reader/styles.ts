10dd602e06e3e1a51b78080bf11e93b4de3a52c2
Gabriel Ávila <gabrielavila04@protonmail.com>
2023-07-14 23:44:41 -0300

    feat: :sparkles: add page

--- next-ts/app/components/Reader/styles.ts
+++ next-ts/app/components/Reader/styles.ts
@@ -1,7 +1,11 @@
-import { Space, scrolls } from "@geavila/gt-design";
+import { Space, scrolls, shadows } from "@geavila/gt-design";
 import styled from "styled-components";
 
 export const ReadContent = styled(Space.Modifiers)`
   display: initial;
+  padding: 1rem;
+  background-color: var(--secondary);
+  border-radius: 5px;
+  ${shadows.basic};
   ${scrolls.default};
 `;
