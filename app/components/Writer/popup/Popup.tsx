57f3d2275c337e046193dddec19ee0aeebad25d4
Gabriel Ávila <gabrielavila04@protonmail.com>
2023-08-15 10:48:13 -0300

    feat: :sparkles: decorate in one word

--- app/components/Writer/popup/Popup.tsx
+++ app/components/Writer/popup/Popup.tsx
@@ -41,22 +41,26 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
     const anchor = selection.anchorNode?.parentElement;
     const anchorOffset = selection.anchorOffset;
 
-    // @ts-expect-error - it is a valid attribute
-    const extent = selection.extentNode?.parentElement;
-    // @ts-expect-error - it is a valid attribute
-    const extentOffset = selection.extentOffset;
+    const focus = selection.focusNode?.parentElement;
+    const focusOffset = selection.focusOffset;
+
+    const areTheSame = anchor === focus;
+
+    // if are the same, checks if the anchorOffset is less than the focusOffset
+    const isAnchorOffsetLessThanFocusOffset =
+      areTheSame && anchorOffset < focusOffset;
 
     // see which node is the first
-    // if the anchor is the first, then the extent is the last
+    // if the anchor is the first, then the focus is the last
     const anchorComesFirst =
-      anchor?.compareDocumentPosition(extent) &
-      Node.DOCUMENT_POSITION_FOLLOWING;
+      anchor?.compareDocumentPosition(focus) &
+        Node.DOCUMENT_POSITION_FOLLOWING || isAnchorOffsetLessThanFocusOffset;
 
-    const firstNode = anchorComesFirst ? anchor : extent;
-    const lastNode = anchorComesFirst ? extent : anchor;
+    const firstNode = anchorComesFirst ? anchor : focus;
+    const lastNode = anchorComesFirst ? focus : anchor;
 
-    const firstNodeOffset = anchorComesFirst ? anchorOffset : extentOffset;
-    const lastNodeOffset = (anchorComesFirst ? extentOffset : anchorOffset) - 1;
+    const firstNodeOffset = anchorComesFirst ? anchorOffset : focusOffset;
+    const lastNodeOffset = (anchorComesFirst ? focusOffset : anchorOffset) - 1;
 
     const firstNodeId = parseInt(firstNode?.getAttribute("data-block-id"));
 
@@ -96,24 +100,27 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
     const wordsAfterSelected = [];
 
     let wordsAreBefore = true;
+    let wordsSelected = [];
 
-    const wordsSelected = text.filter((item) => {
-      const { id } = item;
+    if (selected.length) {
+      wordsSelected = text.filter((item) => {
+        const { id } = item;
 
-      const isSelected = selected.some(
-        ({ id: selectedId }) => selectedId === id
-      );
+        const isSelected = selected.some(
+          ({ id: selectedId }) => selectedId === id
+        );
 
-      if (isSelected) wordsAreBefore = false;
+        if (isSelected) wordsAreBefore = false;
 
-      if (wordsAreBefore) {
-        wordsBeforeSelected.push(item);
-      } else if (!isSelected) {
-        wordsAfterSelected.push(item);
-      }
+        if (wordsAreBefore) {
+          wordsBeforeSelected.push(item);
+        } else if (!isSelected) {
+          wordsAfterSelected.push(item);
+        }
 
-      return isSelected;
-    });
+        return isSelected;
+      });
+    }
 
     // removes the first and last selected
     const selectedWithoutFirstAndLast = wordsSelected
@@ -130,24 +137,67 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
     const newWords = [...wordsBeforeSelected];
 
     // now checks if the first word will be sliced, if so, it'll generate 2 words
-    if (selected?.[0]?.index !== 0) {
+    if (selected?.[0]?.index !== 0 && selected?.[0]?.index != null) {
       const firstWord = wordsSelected[0];
-      const firstWordSliced = {
-        ...firstWord,
-        value: firstWord.value.slice(0, selected[0].index),
-      };
 
-      const firstWordSliced2 = {
-        ...firstWord,
-        value: firstWord.value.slice(selected[0].index),
-        options: firstWord.options.includes("bold")
-          ? firstWord.options
-          : [...firstWord.options, "bold"],
-      };
+      // if areTheSame, then the first word will be sliced in 2
+      if (areTheSame) {
+        const { first, second, third } = firstWord.value.split("").reduce(
+          (acc, item, index) => {
+            if (index < firstNodeOffset) acc.first += item;
+            else if (index > lastNodeOffset) acc.third += item;
+            else acc.second += item;
 
-      newWords.push(firstWordSliced);
-      newWords.push(firstWordSliced2);
-    } else {
+            return acc;
+          },
+          {
+            first: "",
+            second: "",
+            third: "",
+          }
+        );
+
+        if (first) {
+          newWords.push({
+            ...firstWord,
+            value: first,
+          });
+        }
+
+        if (second) {
+          newWords.push({
+            ...firstWord,
+            value: second,
+            options: firstWord.options.includes("bold")
+              ? firstWord.options
+              : [...firstWord.options, "bold"],
+          });
+        }
+
+        if (third) {
+          newWords.push({
+            ...firstWord,
+            value: third,
+          });
+        }
+      } else {
+        const firstWordSliced = {
+          ...firstWord,
+          value: firstWord.value.slice(0, selected[0].index),
+        };
+
+        const firstWordSliced2 = {
+          ...firstWord,
+          value: firstWord.value.slice(selected[0].index),
+          options: firstWord.options.includes("bold")
+            ? firstWord.options
+            : [...firstWord.options, "bold"],
+        };
+
+        newWords.push(firstWordSliced);
+        newWords.push(firstWordSliced2);
+      }
+    } else if (selected?.[0]?.index != null) {
       newWords.push({
         ...wordsSelected[0],
         options: wordsSelected[0].options.includes("bold")
@@ -161,8 +211,10 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
 
     // now checks if the last word will be sliced
     if (
-      selected[selected.length - 1].index !==
-      wordsSelected[wordsSelected.length - 1].value.length - 1
+      selected[selected.length - 1]?.index !==
+        wordsSelected?.[wordsSelected.length - 1]?.value?.length - 1 &&
+      selected[selected.length - 1]?.index != null &&
+      selected[selected.length - 1]?.id !== selected?.[0]?.id
     ) {
       const lastWord = wordsSelected[wordsSelected.length - 1];
 
@@ -181,7 +233,10 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
 
       newWords.push(lastWordSliced);
       newWords.push(lastWordSliced2);
-    } else {
+    } else if (
+      selected[selected.length - 1]?.index != null &&
+      selected[selected.length - 1]?.id !== selected?.[0]?.id
+    ) {
       newWords.push({
         ...wordsSelected[wordsSelected.length - 1],
         options: wordsSelected[wordsSelected.length - 1].options.includes(
@@ -239,7 +294,6 @@ const Popup = memo(({ id, text, parentRef }: IPopup) => {
       };
     });
 
-    console.log(text, newText);
     handleUpdate(id, newText);
   }, [handleUpdate, id, mimic, text, toast]);
 
