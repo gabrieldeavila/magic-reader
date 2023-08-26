4998d0472bf2440a5cfba6e23ef15ab017218232
Gabriel Ávila <gabrielavila04@protonmail.com>
2023-08-25 22:02:09 -0300

    fix: :bug: try deploying old eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,16 @@
-/* eslint-disable no-undef */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:react/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
   },
-  plugins: [
-    "react",
-    "@typescript-eslint",
-    "react-hooks",
-  ],
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
