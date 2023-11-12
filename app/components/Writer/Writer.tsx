1e147319005a145103ebcfb27a3d20a33a18bdb2
Gabriel Ávila <gabrielavila04@protonmail.com>
2023-11-11 23:06:49 -0300

    feat: :sparkles: add loader to the writer view

--- app/components/Writer/Writer.tsx
+++ app/components/Writer/Writer.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { IWriter } from "./interface";
+"use client";
+
+import { Skeletons } from "@geavila/gt-design";
+import useIsSSR from "../../hooks/useIsSSR";
 import WriterContextProvider from "./context/WriterContext";
+import { IWriter } from "./interface";
 
 function Writer({ content }: IWriter) {
+  const { isSSR } = useIsSSR();
+
+  if (isSSR) {
+    return <Skeletons.Canary />;
+  }
+
   return <WriterContextProvider name="writter" initialContent={content} />;
 }
 
