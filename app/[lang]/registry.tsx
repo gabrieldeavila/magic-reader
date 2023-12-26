c37f6c9111e996f275ef0c0cfb323e70c3b58155
Gabriel Ávila <gabrielavila04@protonmail.com>
2023-12-25 21:08:47 -0300

    fix: :ambulance: remove experimental

--- app/[lang]/registry.tsx
+++ app/[lang]/registry.tsx
@@ -15,7 +15,6 @@ export default function StyledComponentsRegistry({
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
-    // @ts-expect-error - do later
     styledComponentsStyleSheet.instance.clearTag();
     return <>{styles}</>;
   });
